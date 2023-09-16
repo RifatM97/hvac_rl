61e3a9b9ff57f052829ef3be469e46228c13ebf7
RifatM97 <ricioman.it@gmail.com>
2023-09-16 12:53:40 +0100

    push local changes

---
+++ HV-Ai-C/hvac_cont.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v "/Users/rifatmahammod/Library/CloudStorage/OneDrive-Personal/Vodafone/Third Placement/hvac_rl/":/hvac_workspace -p 8080:8080 -p 6006:6006 --ulimit memlock=-1 --ulimit stack=67108864 797217000392
